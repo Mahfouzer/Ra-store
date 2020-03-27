c368a4887e76eb7b18d85d4520dd385f79794bf1
mahfouzer <mahfouzs96@gmail.com>
2020-03-27 18:04:15 +0600

    about &sets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
-import "./App.css";
+
+//Styles
 import "./styles/index.scss";
+
+//Components
 import Header from "./components/Header";
+import ShowSet from "./components/ShowSet";
 import Navigation from "./components/Navigation";
+
+//images
 import Logo from "./assets/images/logo.png";
 import CartImg from "./assets/images/cart.png";
+import storeLogo from "./assets/images/storelogo.png";
+import about from "./assets/images/about.png";
+
+//mock data
+import stores from "./mockData/store.json";
+import news from "./mockData/news.json";
 
 function App() {
   document.title = "RA sports";
+
+  const renderSet = set => {
+    return (
+      <section className="flexed">
+        {set.map((aSet, index) => (
+          <ShowSet
+            key={aSet.id}
+            imgSrc={aSet.imgSrc}
+            logoSrc={storeLogo}
+            buttonName={aSet.buttonName}
+            index={index}
+            summary={aSet.summary}
+            findOutMore={aSet.findOutMore}
+          />
+        ))}
+      </section>
+    );
+  };
   return (
     <div className="App">
       <Header
@@ -33,8 +63,24 @@ function App() {
           isLogedIn
         />
       </Header>
+      <main>
+        {renderSet(stores)}
+        <div className="flexed within-container about ">
+          <img src={about} style={{ width: "50%", margin: "50px 10px" }}></img>
+          <p>lol</p>
+        </div>
+        {renderSet(news)}
+      </main>
     </div>
   );
 }
 
 export default App;
+
+//
+//  "https://www9.0zz0.com/2020/03/27/16/916075936.png"
+//  "https://www9.0zz0.com/2020/03/27/16/368053602.png"
+//  "https://www9.0zz0.com/2020/03/27/16/563186283.png"
+//     "https://www9.0zz0.com/2020/03/27/16/887292467.png"
+//     "https://www9.0zz0.com/2020/03/27/16/391678468.png"
+//
---
+++ src/components/ShowSet.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function ShowSet({
+  imgSrc,
+  logoSrc,
+  buttonName,
+  summary,
+  findOutMore,
+  index
+}) {
+  window.summary = summary;
+  return (
+    <section className="showSet">
+      <div
+        className={`flexed ${
+          index === 2 || index === 1 ? "flexed--reversed" : ""
+        }`}
+      >
+        <img src={imgSrc} alt={summary[0]} className={`showSet__img`} />
+        <div
+          className={`showSet__text ${
+            index === 1 || index === 2 ? "showSet__text--reversed" : ""
+          }`}
+        >
+          {findOutMore && <img src={logoSrc} alt="store logo" />}
+          {summary.map((summaryItem, i) => (
+            <p
+              className={
+                summary.length > 1 && i === 0 && "showSet__text__headline"
+              }
+            >
+              {summaryItem}
+            </p>
+          ))}
+          <div>
+            <button>{buttonName}</button> {findOutMore && <a>our story</a>}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
